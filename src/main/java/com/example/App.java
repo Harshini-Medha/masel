d43ebafeea3cc7a497a6c5340bae5b1d6e00518d
Harshini-Medha <109570543+Harshini-Medha@users.noreply.github.com>
2026-06-18 21:43:11 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,33 +1,48 @@
 package com.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main(String[] args)
-    {
-        WebDriver driver = new ChromeDriver();
-        
-        
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+public class App {
 
-        driver.findElement(By.id("user-name"))
-              .sendKeys("standard_user");
+    public static void main(String[] args) {
 
-        driver.findElement(By.id("password"))
-              .sendKeys("secret_sauce");
+        try {
 
-        driver.findElement(By.id("login-button"))
-              .click();
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--remote-allow-origins=*");
+
+            WebDriver driver = new ChromeDriver(options);
+
+            driver.get("https://www.saucedemo.com/");
+
+            driver.findElement(By.id("user-name"))
+                    .sendKeys("standard_user");
+
+            driver.findElement(By.id("password"))
+                    .sendKeys("secret_sauce");
+
+            driver.findElement(By.id("login-button"))
+                    .click();
+
+            System.out.println("TITLE AFTER LOGIN: "
+                    + driver.getTitle());
+
+            driver.quit();
+
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
-
-
-
